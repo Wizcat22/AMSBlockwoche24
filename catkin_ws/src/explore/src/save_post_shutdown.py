6168f5fc0843c6bbb5955580c03db86eb89ce736
Aydin Seven <aysev100@hhu.de>
2024-11-21 12:22:15 +0100

    Feat: Add post-shutdown map saver

---
+++ catkin_ws/src/explore/src/save_post_shutdown.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import rospy
+import subprocess
+
+def shutdown_hook():
+    rospy.loginfo("Saving map!")
+    subprocess.check_call(['rosrun','map_server','map_saver','-f ~/catkin_ws/src/explore/maps/map'])
+
+try:
+    rospy.on_shutdown(shutdown_hook)
+
+except rospy.ROSInterruptException:
+    rospy.loginfo("Interrupt received!")
