9c8b2fedea3eb640279a3b495b6e58a424c85235
Aydin Seven <aysev100@hhu.de>
2024-11-21 15:21:19 +0100

    Update save_post_shutdown.py

--- catkin_ws/src/explore/scripts/save_post_shutdown.py
+++ catkin_ws/src/explore/scripts/save_post_shutdown.py
@@ -10,8 +10,11 @@ def shutdown_hook():
     subprocess.run(['rosrun','map_server','map_saver','-f',path])
     print(subprocess.list2cmdline(['rosrun','map_server','map_saver','-f',path]))
 
-try:
-    rospy.on_shutdown(shutdown_hook)
 
-except rospy.ROSInterruptException:
-    rospy.loginfo("Interrupt received!")
+if __name__=="__main__":
+    rospy.on_shutdown(shutdown_hook)
+    try:
+        rospy.spin()
+
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Interrupt received!")
