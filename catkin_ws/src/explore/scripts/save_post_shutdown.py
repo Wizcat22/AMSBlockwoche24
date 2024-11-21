9f18d26ba059ab32c982f2ec3ad7e8a572d1ed50
Aydin Seven <aysev100@hhu.de>
2024-11-21 15:01:10 +0100

    Update save_post_shutdown.py

--- catkin_ws/src/explore/scripts/save_post_shutdown.py
+++ catkin_ws/src/explore/scripts/save_post_shutdown.py
@@ -5,7 +5,8 @@ import subprocess
 
 def shutdown_hook():
     rospy.loginfo("Saving map!")
-    subprocess.check_call(['rosrun','map_server','map_saver','-f ~/ams-bw-ws2425/catkin_ws/src/explore/maps/map'])
+    subprocess.run(['rosrun','map_server','map_saver','-f','~/ams-bw-ws2425/catkin_ws/src/explore/maps/map'])
+    print(subprocess.list2cmdline(['rosrun','map_server','map_saver','-f','~/ams-bw-ws2425/catkin_ws/src/explore/maps/map']))
 
 try:
     rospy.on_shutdown(shutdown_hook)
